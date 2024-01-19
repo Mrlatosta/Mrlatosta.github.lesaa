4f9cb83abd847c92e4f9ed87fb7a98a807302525
Ray <raymundolarasandoval@gmail.com>
2024-01-18 21:56:34 -0500

    NavBar

---
+++ index.js
@@ -0,0 +1,50 @@
+
+	window.sr = ScrollReveal();
+
+
+		sr.reveal('#sob', {
+				duration: 1000,
+				origin: 'bottom',
+				distance: '-50px'
+
+
+		});
+
+
+		sr.reveal('#ray', {
+				duration: 1000,
+				origin: 'left',
+				distance: '-50px'
+
+
+		});
+
+		sr.reveal('#uno', {
+				duration: 1500,
+				origin: 'right',
+				distance: '-50px'
+
+
+		});
+
+		sr.reveal('#dos', {
+				duration: 1200,
+				origin: 'left',
+				distance: '-50px'
+
+
+		});
+
+		sr.reveal('#imagenray', {
+				duration: 1000,
+				origin: 'left',
+				distance: '-50px'
+
+
+		});
+
+
+
+
+
+		
