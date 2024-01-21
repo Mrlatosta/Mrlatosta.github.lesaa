8ced5758ce3982eb751a1f7eb53ffbbf54d16cda
Ray <raymundolarasandoval@gmail.com>
2024-01-21 00:33:58 -0500

    Scroll

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 
 		sr.reveal('#sob', {
-				duration: 1000,
+				duration: 1500,
 				origin: 'bottom',
 				distance: '-50px'
 
@@ -27,6 +27,40 @@
 
 		});
 
+
+		sr.reveal('#primero', {
+				duration: 1800,
+				origin: 'right',
+				distance: '-50px'
+
+
+		});
+
+		sr.reveal('#segundo', {
+				duration: 2000,
+				origin: 'right',
+				distance: '-50px'
+
+
+		});
+
+sr.reveal('#tercero', {
+				duration: 2200,
+				origin: 'right',
+				distance: '-50px'
+
+
+		});
+
+sr.reveal('#cuarto', {
+				duration: 2400,
+				origin: 'right',
+				distance: '-50px'
+
+
+		});
+
+
 		sr.reveal('#dos', {
 				duration: 1200,
 				origin: 'left',
@@ -42,6 +76,8 @@
 
 
 		});
+
+
 
 
 
